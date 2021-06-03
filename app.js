8b670b569bfd1c2102d846a9fb22602ff872e8f3
yurik <yurik.valder@gmail.com>
2021-06-03 06:06:05 -0400

    adding backend server to deploy

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+const app = express(); 
+
+const port = process.env.PORT || 3000; 
+
+app.listen(port, () => {
+    console.log(`connected on port: ${port}`)
+})
+
+app.use(express.static('public')); 
